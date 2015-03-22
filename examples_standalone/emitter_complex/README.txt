170900f1a3b3fabca6aa5ba1d1d66ed6878040ec
Mark Veltzer <mark.veltzer@gmail.com>
2015-03-22 17:35:02 +0200

    started working on the complex example

---
+++ examples_standalone/emitter_complex/README.txt
@@ -0,0 +1,6 @@
+This example shows how a generator could generate many files and still get an incremental
+build using scons.
+
+Note that in this example the generator is not itself a build target (it is a python
+script that requires no building).
+that is why this example is called simple.
---
+++ examples_standalone/emitter_complex/list_generated_files.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+this is a script with no dependencies that knows how to get the source file and emit
+a list of the generated files.
+'''
+
+import sys # for stdin
+import re # for match
+
+for line in sys.stdin:
+	if re.match('^\w+:\s*', line):
+		line=line.rstrip()
+		line=line[:-1]
+		print('gen/'+line+'.cc')
+		print('gen/'+line+'.hh')
