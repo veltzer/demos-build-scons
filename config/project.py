e3d20277320fabd603c8693a0d6b89e7479f7877
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:58 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-scons"
-project_long_description = "Demos for the scons build tool"
-project_year_started = 2015
-project_description = "Demos for the scons build tool"
+description_short = "Demos for the scons build tool"
